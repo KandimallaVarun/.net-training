2260629c4950c110f4c78f6f1d701a540915e544
KandimallaVarun <varunkandimalla333@gmail.com>
2026-06-30 22:58:53 +0530

    creted word for .net ecosystem

---
+++ Day-1_.NET_Ecosystem/ClassSolution1/ClassLibrary1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ Day-1_.NET_Ecosystem/NamespaceEx/NamespaceEx/Program.cs
@@ -0,0 +1,40 @@
+﻿using NamespaceEx.TowerA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamespaceEx
+{
+    namespace TowerA
+    {
+        public class SecondFloor
+        {
+            public static void RoomNo2()
+            {
+                Console.WriteLine("TowerA-SecondFloor-RoomNo2");
+            }
+        }
+    }
+    namespace TowerB
+    {
+        public class SecondFloor
+        {
+            public static void RoomNo2()
+            {
+                Console.WriteLine("TowerB-SecondFloor-RoomNo2");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TowerA.SecondFloor.RoomNo2(); // Calls TowerA.SecondFloor.RoomNo2() due to using directive
+            //TowerB.SecondFloor.RoomNo2(); // Calls TowerB.SecondFloor.RoomNo2() explicitly
+
+            SecondFloor.RoomNo2();
+        }
+    }
+}
