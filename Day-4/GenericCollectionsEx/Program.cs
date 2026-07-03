cda6e03ec75471fa3b7d67b9c1d2089cf8fcf85d
KandimallaVarun <varunkandimalla333@gmail.com>
2026-07-03 23:14:03 +0530

    day-5 : LINQ, Quantifiers

---
+++ Day-4/GenericCollectionsEx/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollectionsEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            list.Remove(20);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+
+            dict.Add(1, "One");
+            dict.Add(2, "Two");
+            dict.Add(3, "Three");
+            dict.Remove(2);
+            //dict = dict.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine(item.Key + " : " + item.Value);
+            }
+
+            foreach(var item in dict.Keys)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in dict.Values)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+            var x = 10;
+            Console.WriteLine(x.GetType());// how to check the type of variable in c#? use GetType() method
+
+            dynamic y;
+            y = 10L;
+            Console.WriteLine(y.GetType()+" : "+y);
+            y = "Hello";
+            Console.WriteLine(y.GetType());
+        }
+    }
+}
---
+++ Day-4/NonGenericCollectionsEx/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonGenericCollectionsEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+
+            al.Add(10);
+            al.Add(20);
+            al.Add("Hello");
+            al.Add(30.5);
+
+            foreach (var item in al)
+            {
+                Console.WriteLine(item);
+            }
+
+            Hashtable ht = new Hashtable();
+
+            ht.Add(1, "One");
+            ht.Add(2, "Two");
+            ht.Add(3, 333);
+            ht.Add(4, 4.5);
+
+            foreach (DictionaryEntry item in ht)
+            {
+                Console.WriteLine(item.Key + " : " + item.Value);
+            }
+
+            Stack st = new Stack();
+
+            st.Push(1);
+            st.Push(2);
+            st.Pop();
+            st.Push(3);
+            Console.WriteLine(st.Peek());
+
+            foreach (var item in st)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
